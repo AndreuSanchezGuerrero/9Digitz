2648c17db75aa247eeecbf64797402c78a5c27b7
Andreu Sánchez Guerrero <123182864+AndreuSanchezGuerrero@users.noreply.github.com>
2025-09-11 13:31:47 +0200

    Fix JSX closing tag error

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -73,7 +73,7 @@ const LanguageSelector: React.FC = () => {
                   )}
                 </motion.button>
               ))}
-            </div>
+            </motion.div>
           </>
         )}
       </AnimatePresence>
