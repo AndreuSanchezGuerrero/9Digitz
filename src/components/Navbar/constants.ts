77297775df2b72c0a6343dc2bfaab6965597246d
Andreu Sánchez Guerrero <123182864+AndreuSanchezGuerrero@users.noreply.github.com>
2025-09-11 12:57:20 +0200

    Create missing Navbar constants file

---
+++ src/components/Navbar/constants.ts
@@ -0,0 +1,11 @@
+import { NavItem } from './types';
+
+export const NAVBAR_CONSTANTS = {
+  NAV_ITEMS: [
+    { label: 'Services', href: '#services' },
+    { label: 'Security', href: '#security' },
+    { label: 'Process', href: '#process' },
+    { label: 'Team', href: '#team' },
+    { label: 'Contact', href: '#contact' }
+  ] as NavItem[]
+};
